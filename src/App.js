a2bdc4113b29b6df8af162dd7255db3f66ec1201
Adam Lnenicka <adam.lnenicka123@gmail.com>
2021-10-03 12:16:19 +0100

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react'
+// import Day from './components/Day'
 
 function App() {
+
+  const [weatherInfo, setWeatherInfo] = useState({})
+
+  const showWeather = async (e) =>{
+
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=f795afb3d4db7993e671d6dc9352d53d"
+    try{    const response = await fetch(url)
+      const data = await response.json()
+      console.log(data)
+      if (response.status === 200) {
+        setWeatherInfo(data)
+      } else {
+        throw new Error("Something went wrong!");
+      }
+    }
+      catch(err){
+        console.error(err)
+      }
+}
+
+useEffect(() => {
+  showWeather()
+},[]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {!(Object.keys(weatherInfo).length === 0 && weatherInfo.constructor === Object) ?
+          <div>
+            <div> {weatherInfo.base} </div>
+            <div> {weatherInfo.wind.speed} </div>
+            <div> {weatherInfo.main.temp} </div>
+            <div> {weatherInfo.weather[0].description} </div>
+          </div>
+          : <></>
+      }
     </div>
+
   );
 }
 
